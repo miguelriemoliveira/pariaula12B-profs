9a695199b11296896e7e0203640fa46dce4bf726
Miguel Armando Riem Oliveira <m.riem.oliveira@gmail.com>
2019-12-11 17:02:30 +0000

    #2 for now just a copy of the regular polygon function code

--- src/auxcallbacks.c
+++ src/auxcallbacks.c
@@ -93,7 +93,65 @@ void pari_draw_regularpolygon(GdkEvent *event)
  */
 void pari_draw_starpolygon(GdkEvent *event)
 {
-	//...
+    static int last_button=0;
+    GdkEventButton *geb=(GdkEventButton *)event; //aux variable
+    switch( event->type)
+    {
+        case GDK_BUTTON_PRESS: //it's a new one
+            last_button=geb->button;
+            if( geb->button == 1 )  //left button
+            {
+                double x=geb->x;
+                double y=geb->y;
+                p_polygon *npoly=(p_polygon *)malloc(sizeof(p_polygon));
+                npoly->x=(double*)malloc((statusG.regularpolygon_vertices)*sizeof(double));
+                npoly->y=(double*)malloc((statusG.regularpolygon_vertices)*sizeof(double));
+                double ang=2*M_PI/statusG.regularpolygon_vertices;
+                double radius=20;
+                for(int n=0; n< statusG.regularpolygon_vertices;n++)
+                {
+                    double x1=x+radius*cos( n*ang );
+                    double y1=y+radius*sin( n*ang );
+                    npoly->x[n]=x1;
+                    npoly->y[n]=y1;
+                }
+                npoly->num_points=statusG.regularpolygon_vertices;  //to close the polygon
+                npoly->x0=x;
+                npoly->y0=y;
+                npoly->line_color=0;
+                npoly->fill_color=statusG.regularpolygon_rgbcolor;
+
+                pari_add_poly_to_main_drawing(npoly, pdG);
+            }
+
+            break;
+
+        case GDK_MOTION_NOTIFY:  //mouse motion means modifying one just drawn
+            if( last_button == 1 )  //if last pressed button was the right one (avoid motion from other buttons)
+            {
+                double x=geb->x;
+                double y=geb->y;
+                double x0=pdG->polys[pdG->num_poly-1]->x0;
+                double y0=pdG->polys[pdG->num_poly-1]->y0;
+                p_polygon *npoly=pdG->polys[pdG->num_poly-1];
+                double ang=2*M_PI/statusG.regularpolygon_vertices;
+                double radius=sqrt((x-x0)*(x-x0)+(y-y0)*(y-y0));
+                double angoff=atan2( y-y0, x-x0);
+                for(int n=0; n < statusG.regularpolygon_vertices;n++)
+                {
+                    double x1=x0+radius*cos( n*ang+angoff );
+                    double y1=y0+radius*sin( n*ang+angoff );
+                    npoly->x[n]=x1;
+                    npoly->y[n]=y1;
+                }
+            }
+
+            break;
+
+        default:
+            //Does nothing
+            break;
+    }
 }
 
 /**
