8adb439718029a9499a83c00fcb3e9d75a1f8a8a
Miguel Armando Riem Oliveira <m.riem.oliveira@gmail.com>
2019-12-11 17:33:20 +0000

    close #2. works though still has a glitch for odd number of vertices ...

--- src/auxcallbacks.c
+++ src/auxcallbacks.c
@@ -98,28 +98,40 @@ void pari_draw_starpolygon(GdkEvent *event)
     switch( event->type)
     {
         case GDK_BUTTON_PRESS: //it's a new one
+            printf("button press\n");
             last_button=geb->button;
             if( geb->button == 1 )  //left button
             {
+//                statusG.regularpolygon_vertices = 55;
+                printf("number of vertices is %d", statusG.starpolygon_vertices);
                 double x=geb->x;
                 double y=geb->y;
                 p_polygon *npoly=(p_polygon *)malloc(sizeof(p_polygon));
-                npoly->x=(double*)malloc((statusG.regularpolygon_vertices)*sizeof(double));
-                npoly->y=(double*)malloc((statusG.regularpolygon_vertices)*sizeof(double));
-                double ang=2*M_PI/statusG.regularpolygon_vertices;
+                npoly->x=(double*)malloc((statusG.starpolygon_vertices)*sizeof(double));
+                npoly->y=(double*)malloc((statusG.starpolygon_vertices)*sizeof(double));
+                double ang=2*M_PI/statusG.starpolygon_vertices;
                 double radius=20;
-                for(int n=0; n< statusG.regularpolygon_vertices;n++)
-                {
-                    double x1=x+radius*cos( n*ang );
-                    double y1=y+radius*sin( n*ang );
+                for(int n=0; n< statusG.starpolygon_vertices;n++) {
+
+                    double x1, y1;
+                    if (n % 2 == 0) // number is even
+                    {
+                        x1 = x + radius * cos(n * ang);
+                        y1 = y + radius * sin(n * ang);
+                    } else {
+                        x1=x+radius/2*cos( n*ang);
+                        y1=y+radius/2*sin( n*ang);
+                    }
+
                     npoly->x[n]=x1;
                     npoly->y[n]=y1;
+                    printf("my pt is x=%f and y=%f\n", x1, y1);
                 }
-                npoly->num_points=statusG.regularpolygon_vertices;  //to close the polygon
+                npoly->num_points=statusG.starpolygon_vertices;  //to close the polygon
                 npoly->x0=x;
                 npoly->y0=y;
                 npoly->line_color=0;
-                npoly->fill_color=statusG.regularpolygon_rgbcolor;
+                npoly->fill_color=statusG.starpolygon_rgbcolor;
 
                 pari_add_poly_to_main_drawing(npoly, pdG);
             }
@@ -134,10 +146,10 @@ void pari_draw_starpolygon(GdkEvent *event)
                 double x0=pdG->polys[pdG->num_poly-1]->x0;
                 double y0=pdG->polys[pdG->num_poly-1]->y0;
                 p_polygon *npoly=pdG->polys[pdG->num_poly-1];
-                double ang=2*M_PI/statusG.regularpolygon_vertices;
+                double ang=2*M_PI/statusG.starpolygon_vertices;
                 double radius=sqrt((x-x0)*(x-x0)+(y-y0)*(y-y0));
                 double angoff=atan2( y-y0, x-x0);
-                for(int n=0; n < statusG.regularpolygon_vertices;n++)
+                for(int n=0; n < statusG.starpolygon_vertices;n++)
                 {
                     double x1=x0+radius*cos( n*ang+angoff );
                     double y1=y0+radius*sin( n*ang+angoff );
